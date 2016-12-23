4440090c058736f30d5337675fbd207f683aef7d
HomeACcessoryKid <homeaccessorykid@gmail.com>
2016-12-23 14:50:05 +0100

    First files to test git procedures

---
+++ hkc/include/hkc/user_settings.h
@@ -0,0 +1,15 @@
+//#define WOLFSSL_MISC_INCLUDED
+#define FREERTOS
+#define WOLFSSL_LWIP
+#define NO_WOLFSSL_MEMORY
+#define WOLFCRYPT_HAVE_SRP
+#define NO_MD5
+#define NO_SHA
+#define WOLFSSL_SHA512
+#define WOLFSSL_SMALL_STACK
+#define HAVE_HKDF
+#define HAVE_CHACHA
+#define HAVE_POLY1305
+#define HAVE_ED25519
+#define HAVE_CURVE25519
+#define CUSTOM_RAND_GENERATE_BLOCK os_get_random
